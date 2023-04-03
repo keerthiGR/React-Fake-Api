9970e54a943b45bb56faa9d32ae8fbd2335f2256
unknown <keerthiyadhav85@gmail.com>
2023-04-03 12:46:21 +0530

    Updates

---
+++ precache-manifest.ac43f24114c418cf27199ff30d410221.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4d68d6c2925462e56dd",
+    "url": "/React-Fake-Api/static/css/main.e3fab19b.chunk.css"
+  },
+  {
+    "revision": "b4d68d6c2925462e56dd",
+    "url": "/React-Fake-Api/static/js/main.b4d68d6c.chunk.js"
+  },
+  {
+    "revision": "3dd6d2256272fb5ca5b0",
+    "url": "/React-Fake-Api/static/js/1.3dd6d225.chunk.js"
+  },
+  {
+    "revision": "fa915ef11be6a32c96db",
+    "url": "/React-Fake-Api/static/js/runtime~main.fa915ef1.js"
+  },
+  {
+    "revision": "08716d417fd6cf3e01ecac94baee2413",
+    "url": "/React-Fake-Api/index.html"
+  }
+];
